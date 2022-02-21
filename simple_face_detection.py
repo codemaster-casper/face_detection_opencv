63cc80f966fd84e172399e37c31252b30ba78fa9
Gourav <89159624+codemaster-casper@users.noreply.github.com>
2022-02-21 15:48:20 +0530

    Update simple_face_detection.py

--- simple_face_detection.py
+++ simple_face_detection.py
@@ -10,24 +10,24 @@ face_cascade_classifire = cv2.CascadeClassifier("haarcascade_frontalface_default
 #capting voideo in an object. 0 attribute defines the default camera source
 cap = cv2.VideoCapture(0)
 
-#Video is a continous sequence of franes. Creating infinite loop to show to frames continously. 
+#Video is a continous sequence of frames. Creating infinite loop to show to frames continously. 
 while True:
 
-    #Readtingg the video source object and storing each frame in the "frame"
+    #Reading the video source object and storing each frame in the "frame"
     #"success" returns a boolean value for the frame storage operation
     sucess, frame = cap.read()
 
     #Converting Color frame to gray, since the classifire works with BnW only
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     
-    #implementing the face cascade classifire to the gray franmes
+    #implementing the face cascade classifire to the gray frames
     faces = face_cascade_classifire.detectMultiScale(gray_frame,1.1,4)
 
     #creating a bounding box around the face and passing original frame "frame" in argument
     for (x,y,w,h) in faces:
         cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),2)
 
-    #Displaying frame by frame in a window a video
+    #Displaying frame by frame in a window as video
     cv2.imshow("Video", frame)
 
     #dislplaying the each frame for 1ms and quitting by pressing "q" 
